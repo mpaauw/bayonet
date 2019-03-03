5389319af49935241eb806984213fbc5c2c13817
mpaauw <matt.paauw@gmail.com>
2019-03-02 22:49:08 -0800

    Add unit tests, fixtures, generators for GetItemCommand.

---
+++ test/bayonet.Api.Tests/Generators.cs
@@ -0,0 +1,30 @@
+﻿using bayonet.Core.Models;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bayonet.Api.Tests
+{
+    public static class Generators
+    {
+        public static Faker<Item> FakeItem()
+        {
+            return new Faker<Item>()
+                .RuleFor(t => t.Id, f => f.Lorem.Word())
+                .RuleFor(t => t.Deleted, f => f.Random.Bool())
+                .RuleFor(t => t.Type, ItemType.Story)
+                .RuleFor(t => t.By, f => f.Lorem.Word())
+                .RuleFor(t => t.Time, f => f.Random.Int())
+                .RuleFor(t => t.Text, f => f.Hacker.Phrase())
+                .RuleFor(t => t.Dead, f => f.Random.Bool())
+                .RuleFor(t => t.Parent, f => f.Random.Int())
+                .RuleFor(t => t.Poll, f => f.Random.Int())
+                .RuleFor(t => t.Kids, f => f.Lorem.Words())
+                .RuleFor(t => t.Url, f => f.Internet.Url())
+                .RuleFor(t => t.Score, f => f.Random.Int())
+                .RuleFor(t => t.Parts, f => f.Lorem.Words())
+                .RuleFor(t => t.Descendants, f => f.Random.Int());
+        }
+    }
+}
---
+++ test/bayonet.Api.Tests/Items/GetItemCommandFixture.cs
@@ -0,0 +1,70 @@
+﻿using AndyC.Patterns.Commands;
+using bayonet.Api.Commands.Items;
+using bayonet.Core.Common;
+using bayonet.Core.Models;
+using bayonet.Data;
+using Bogus;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bayonet.Api.Tests.Items
+{
+    public class GetItemCommandFixture
+    {
+        private readonly IWebService webService;
+        private readonly ICommandRouter commandRouter;
+        private readonly Faker faker;
+
+        private string id;
+
+        public GetItemCommandFixture()
+        {
+            this.webService = A.Fake<IWebService>();
+            this.commandRouter = A.Fake<ICommandRouter>();
+            this.faker = new Faker();
+        }
+
+        public GetItemCommandFixture WithValidId()
+        {
+            this.id = this.faker.Lorem.Word();
+            return this;
+        }
+
+        public GetItemCommandFixture WithInvalidId()
+        {
+            string[] badData = { "", " ", null };
+            this.id = badData[this.faker.Random.Int(0, 2)];
+            return this;
+        }
+
+        public GetItemCommandFixture WithValidWebServiceGetContentAsyncResponse()
+        {
+            var item = Generators.FakeItem().Generate();
+            A.CallTo(() => this.webService.GetContentAsync<Item>(A<string>._))
+                .Returns(item);
+            return this;
+        }
+
+        public GetItemCommandFixture WithWebServiceGetContentAsyncExceptionEncountered()
+        {
+            A.CallTo(() => this.webService.GetContentAsync<Item>(A<string>._))
+                .Throws(new Exception());
+            return this;
+        }
+
+        public async Task<Result<Item>> ExecuteCommandUnderTest()
+        {
+            var command = GetCommandUnderTest();
+            var handler = new GetItemCommand.Handler(this.webService);
+            return await handler.ExecuteAsync(command);
+        }
+
+        private GetItemCommand GetCommandUnderTest()
+        {
+            return new GetItemCommand(this.id);
+        }
+    }
+}
