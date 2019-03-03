d6ef791548cdbb2c8fb1c4eb3fe6086687404054
mpaauw <matt.paauw@gmail.com>
2019-03-03 14:37:56 -0800

    Add unit tests, fixture for GetUpdatedUsersCommand.

---
+++ test/bayonet.Api.Tests/Users/GetUpdatedUsersCommandFixture.cs
@@ -0,0 +1,75 @@
+﻿using bayonet.Api.Commands.Users;
+using bayonet.Core.Common;
+using bayonet.Core.Models;
+using bayonet.Data;
+using Bogus;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bayonet.Api.Tests.Users
+{
+    public class GetUpdatedUsersCommandFixture
+    {
+        private readonly IWebService webService;
+        private readonly Faker faker;
+
+        private int count;
+
+        public GetUpdatedUsersCommandFixture()
+        {
+            this.webService = A.Fake<IWebService>();
+            this.faker = new Faker();
+        }
+
+        public GetUpdatedUsersCommandFixture WithInvalidCount()
+        {
+            int[] badData = { -1, 0 };
+            this.count = badData[this.faker.Random.Int(0, 1)];
+            return this;
+        }
+
+        public GetUpdatedUsersCommandFixture WithValidCount()
+        {
+            this.count = this.faker.Random.Int(1, 10);
+            return this;
+        }
+
+        public GetUpdatedUsersCommandFixture WithWebServiceGetContentAsyncUpdatesExceptionEncountered()
+        {
+            A.CallTo(() => this.webService.GetContentAsync<Updates>(A<string>._))
+                .Throws(new Exception());
+            return this;
+        }
+
+        public GetUpdatedUsersCommandFixture WithValidWebServiceGetContentAsyncUpdatesResponse()
+        {
+            var fakeUpdates = Generators.FakeUpdates().Generate();
+            A.CallTo(() => this.webService.GetContentAsync<Updates>(A<string>._))
+                .Returns(fakeUpdates);
+            return this;
+        }
+
+        public GetUpdatedUsersCommandFixture WithValidWebServiceGetContentAsyncUserResponse()
+        {
+            var fakeUser = Generators.FakeUser().Generate();
+            A.CallTo(() => this.webService.GetContentAsync<User>(A<string>._))
+                .Returns(fakeUser);
+            return this;
+        }
+
+        public async Task<Result<IEnumerable<User>>> ExecuteCommandUnderTest()
+        {
+            var command = GetCommandUnderTest();
+            var handler = new GetUpdatedUsersCommand.Handler(this.webService);
+            return await handler.ExecuteAsync(command);
+        }
+
+        private GetUpdatedUsersCommand GetCommandUnderTest()
+        {
+            return new GetUpdatedUsersCommand(this.count);
+        }
+    }
+}
---
+++ test/bayonet.Api.Tests/Users/GetUpdatedUsersCommandTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace bayonet.Api.Tests.Users
+{
+    public class GetUpdatedUsersCommandTests
+    {
+        private readonly GetUpdatedUsersCommandFixture fixture;
+
+        public GetUpdatedUsersCommandTests()
+        {
+            this.fixture = new GetUpdatedUsersCommandFixture();
+        }
+
+        [Fact]
+        public async Task Invalid_Count_Should_Return_Bad_Request()
+        {
+            var result = await this.fixture
+                .WithInvalidCount()
+                .WithValidWebServiceGetContentAsyncUpdatesResponse()
+                .WithValidWebServiceGetContentAsyncUserResponse()
+                .ExecuteCommandUnderTest();
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Encountering_Exception_Should_Return_Server_Error()
+        {
+            var result = await this.fixture
+                .WithValidCount()
+                .WithWebServiceGetContentAsyncUpdatesExceptionEncountered()
+                .WithValidWebServiceGetContentAsyncUserResponse()
+                .ExecuteCommandUnderTest();
+            Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Valid_Request_Should_Return_Ok()
+        {
+            var result = await this.fixture
+                .WithValidCount()
+                .WithValidWebServiceGetContentAsyncUpdatesResponse()
+                .WithValidWebServiceGetContentAsyncUserResponse()
+                .ExecuteCommandUnderTest();
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
