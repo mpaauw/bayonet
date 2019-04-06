67f8bc7061bf9d3c402fba19a4c8745af76c257e
mpaauw <matt.paauw@gmail.com>
2019-04-05 22:44:46 -0700

    Add missing tests for story + user functions.

---
+++ test/bayonet.Client.Tests/Functions/Users/UserFunctionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace bayonet.Client.Tests.Functions.Users
+{
+    public class UserFunctionsTests
+    {
+        private readonly UserFunctionsFixture fixture;
+        private readonly Faker faker;
+
+        public UserFunctionsTests()
+        {
+            this.fixture = new UserFunctionsFixture();
+            this.faker = new Faker();
+        }
+
+        [Fact]
+        public async Task Request_For_User_Should_Return_Valid_Result()
+        {
+            string id = this.faker.Lorem.Word();
+            var result = await this.fixture
+                .WithId(id)
+                .ExecuteGetUser();
+            Assert.Equal(id, result.Value.Id);
+        }
+
+        [Fact]
+        public async Task Request_For_Updated_Users_Should_Return_Valid_Result()
+        {
+            int count = this.faker.Random.Int(1, 10);
+            var result = await this.fixture
+                .WithCount(count)
+                .ExecuteGetUpdatedUsers();
+            Assert.Equal(count, result.Value.Count());
+        }
+    }
+}
